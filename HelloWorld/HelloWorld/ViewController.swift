6d1d6040d58c7bce2916202fc71fd97b184a459d
margaritageleta <noticiasmundiales99@gmail.com>
2020-03-16 17:00:51 +0100

    Hello world basic

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Rita on 16/03/2020.
+//  Copyright © 2020 3omni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
